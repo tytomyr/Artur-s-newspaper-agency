14ef1b9f4b942a40577da9de2af7e321c3b86642
Artur Tytomyr <art.tytomyr@gmail.com>
2023-03-29 11:54:33 +0300

    Added new models for redactor, topic and newspaper, migrations ran, auth_user_models in settings redefined

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class NewsPaper(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    content = models.TextField()
+    published_date = models.DateField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    publishers = models.ManyToManyField(Redactor, related_name="newspapers")
+
+    def __str__(self) -> str:
+        return self.title
--- arturs_newspaper_agency/settings.py
+++ arturs_newspaper_agency/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'agency',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "agency.Redactor"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
