37f696353c1c186630b07779e2d2b545380a221e
Artur Tytomyr <art.tytomyr@gmail.com>
2023-03-29 12:45:41 +0300

    Added filled  MVT for agency

---
+++ agency/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+
+from .views import (
+    index,
+    TopicListView,
+    RedactorListView,
+    NewsPaperListView,
+
+)
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("redactors/",
+         RedactorListView.as_view(),
+         name="redactor-list",
+         ),
+    path("topics/",
+         TopicListView.as_view(),
+         name="topic-list",
+         ),
+    path("newspapers/",
+         NewsPaperListView.as_view(),
+         name="newspaper-list",
+         ),
+]
--- agency/views.py
+++ agency/views.py
@@ -1,3 +1,45 @@
+import generic as generic
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.views import generic
+from agency.models import Topic, Redactor, NewsPaper
 
-# Create your views here.
+
+def index(request):
+    num_redactors = Redactor.objects.count()
+    num_newspapers = NewsPaper.objects.count()
+    num_topics = Topic.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_redactors": num_redactors,
+        "num_newspapers": num_newspapers,
+        "num_topics": num_topics,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "agency/index.html", context=context)
+
+
+class TopicListView(generic.ListView):
+    model = Topic
+    context_object_name = "topic_list"
+    template_name = "agency/topic_list.html"
+    queryset = Topic.objects.all()
+
+
+class RedactorListView(generic.ListView):
+    model = Redactor
+    context_object_name = "redactor_list"
+    template_name = "agency/redactor_list.html"
+    queryset = Topic.objects.all()
+
+
+class NewsPaperListView(generic.ListView):
+    model = NewsPaper
+    context_object_name = "newspaper_list"
+    template_name = "agency/newspaper_list.html"
+    queryset = Topic.objects.all()
+
